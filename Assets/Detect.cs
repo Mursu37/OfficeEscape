87c8163e502d22adc08f0edde1200aafabd262b9
Arina Vasileva <arina.vasileva.ed@gmail.com>
2025-01-24 22:33:26 +0200

    test scene
    test scene

---
+++ Assets/Detect.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detect :
+public transform Player;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        // Debug.Log("Find");
+        if (col.gameObject.name == "player")
+        {
+            transform.LookAt(player);
+            transform.Translate(0, 0, 0.3f);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

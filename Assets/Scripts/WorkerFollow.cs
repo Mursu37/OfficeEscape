d00e21bbb056cc57b9890f24e608865160156510
ShadySeal <122457366+ShadySeal@users.noreply.github.com>
2025-01-25 10:05:19 +0200

    mad workers will follow the player in line

--- Assets/Scripts/WorkerFollow.cs
+++ Assets/Scripts/WorkerFollow.cs
@@ -5,7 +5,8 @@ using UnityEngine.AI;
 
 public class WorkerFollow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform player;
+    [SerializeField] private FollowerList followerList;
     private NavMeshAgent agent;
     private LayerMask layerMask;
 
@@ -19,6 +20,28 @@ public class WorkerFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (!followerList.followers.Contains(gameObject))
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+            if (distanceToPlayer < 1f)
+            {
+                followerList.followers.Add(gameObject);
+            }
+        }
+        else
+        {
+            int currentIndex = followerList.followers.IndexOf(gameObject);
+
+            if (currentIndex > 0)
+            {
+                GameObject previousFollower = followerList.followers[currentIndex - 1];
+                agent.SetDestination(previousFollower.transform.position);
+            }
+            else
+            {
+                agent.SetDestination(player.position);
+            }
+        }
+        
     }
 }

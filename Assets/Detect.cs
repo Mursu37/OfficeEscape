539374f72317cf4d5866f7338c8b4dd09f474fc5
Arina Vasileva <arina.vasileva.ed@gmail.com>
2025-01-25 11:07:09 +0200

    updated detection

--- Assets/Detect.cs
+++ Assets/Detect.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class Detect : MonoBehaviour
 {
-    public float detectionDistance = 8f; // Detection range
+    public float detectionDistance = 8f; 
     public Transform player;
+    public LayerMask obstacleLayer; 
+    private bool playerDetected = false;
 
     // Update is called once per frame
     void Update()
@@ -15,29 +17,43 @@ public class Detect : MonoBehaviour
 
         if (distance < detectionDistance)
         {
-            transform.LookAt(player);
+            Vector3 directionToPlayer = (player.position - transform.position).normalized;
+            if (!Physics.Raycast(transform.position, directionToPlayer, detectionDistance, obstacleLayer))
+            {
+                
+                transform.LookAt(player);
 
-            EndGame();
+                if (!playerDetected)
+                {
+                    EndGame();
+                    playerDetected = true; 
+                }
+            }
+        }
+        else
+        {
+            playerDetected = false; 
         }
     }
 
-    
     private void EndGame()
     {
         Debug.Log("Player detected! Game Over.");
-
         // Uncomment the line below to load a game over scene
         // SceneManager.LoadScene("GameOverScene");
 
-        // Or 
+        // Or quit the application
         // Application.Quit();
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.name == "Player")
+        if (other.name == "Player")
         {
-            Debug.Log("Player detected!");
+            Vector3 distVector = other.transform.position - transform.position;
+            distVector.y = 0; // Keep the y component zero for horizontal rotation
+            Quaternion angle = Quaternion.LookRotation(distVector);
+            transform.rotation = Quaternion.Slerp(transform.rotation, angle, Time.deltaTime * 5f); // Smooth rotation
         }
     }
 }

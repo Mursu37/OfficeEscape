cbfba4eb8633c2766c393f69dce4592462c596c1
ShadySeal <122457366+ShadySeal@users.noreply.github.com>
2025-01-25 13:12:28 +0200

    commit

--- Assets/Scripts/WorkerFollow.cs
+++ Assets/Scripts/WorkerFollow.cs
@@ -2,59 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class WorkerFollow : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    [SerializeField] private FollowerList followerList;
-    [SerializeField] private float holdTime;
+    [SerializeField] private AddFollowers addFollowers;
     private NavMeshAgent agent;
-    private LayerMask layerMask;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        layerMask = LayerMask.GetMask("Worker");
-        holdTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!followerList.followers.Contains(gameObject))
+        if (!addFollowers.followers.Contains(gameObject))
         {
-            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-            if (distanceToPlayer < 2f)
-            {
-                if (Input.GetButton("Fire1"))
-                {
-                    RaycastHit hit;
-                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
-                    {
-                        if (hit.collider.CompareTag("Worker"))
-                        {
-                            holdTime += Time.deltaTime;
-                            if (holdTime >= 1f)
-                            {
-                                followerList.followers.Add(gameObject);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    holdTime = 0f;
-                }
-            }
+            addFollowers.HandleFollower(gameObject);
         }
         else
         {
-            int currentIndex = followerList.followers.IndexOf(gameObject);
+            int currentIndex = addFollowers.followers.IndexOf(gameObject);
 
             if (currentIndex > 0)
             {
-                GameObject previousFollower = followerList.followers[currentIndex - 1];
+                GameObject previousFollower = addFollowers.followers[currentIndex - 1];
                 agent.SetDestination(previousFollower.transform.position);
             }
             else
